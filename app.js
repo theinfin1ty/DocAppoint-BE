fe8d9a5fc82f070dab53f472fbd3b8087c8c4a8f
theinfin1ty <anantgaurav1010@gmail.com>
2021-07-01 17:55:28 +0530

    Added functionality on Admin's end to create new user accounts (Admin/Doctor/Client)

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-requier('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,27 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAdmin } = require('../middleware');
+const User = require('../models/user');
 
 router.get('/', isLoggedIn, isAdmin, (req, res) => {
     res.render('admin/index');
 })
+
+router.get('/register', isLoggedIn, isAdmin, (req, res) => {
+    res.render('admin/register');
+})
+
+router.post('/register', isLoggedIn, isAdmin, catchAsync(async (req, res) => {
+    try{
+        const { name, userType, email, password } = req.body;
+        const user = new User({ name: name, email: email, userType: userType, username: email });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Account created successfully');
+        res.redirect('/admin');
+    } catch(e){
+        req.flash('error', e.message);
+        res.redirect('/admin/register');
+    }
+}))
 
 module.exports = router;
