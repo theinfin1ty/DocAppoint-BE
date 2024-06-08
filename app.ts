ab928e27ff91fc122d059d057dff166bdf4bca2c
theinfin1ty <anantgaurav1010@gmail.com>
2024-06-08 15:10:27 +0530

    feat: added dockerfile

--- app.ts
+++ app.ts
@@ -26,10 +26,7 @@ admin.initializeApp({
 });
 
 mongoose
-  .connect(process.env.DB_URL as string, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.DB_URL as string)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log(err));
 
